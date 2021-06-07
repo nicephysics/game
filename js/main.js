6e1c9c07c89c65768b00bf27cf9c69c7a3c31c56
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-07 14:34:43 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 console.log("Hello World!")
+
+var canvas = new fabric.Canvas("canvas")
+console.log(canvas)
+
+function resizeCanvas() {
+  // resize to full window width and height
+  
+}
+
+function test_1() {
+  // create a rectangle object
+  var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20
+  })
+
+  // "add" rectangle onto canvas
+  canvas.add(rect)
+}

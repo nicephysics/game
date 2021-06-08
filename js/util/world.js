874280ab4b32fa89990dc1f282355e5a03ca4b3c
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-08 11:32:29 +0800

    Update world.js

--- js/util/world.js
+++ js/util/world.js
@@ -1,16 +1,20 @@
 var Events = Matter.Events,
     Composite = Matter.Composite
 
-var setGravity = function(engine, x, y) {
+export var util_world = { }
+
+// this(engine, x, y)
+util_world.gravity = function(engine, x, y) {
   engine.gravity.x = x
   engine.gravity.y = y
 }
 
 // don't use this though?
-var addToWorld = function(world, bodies) {
+util_world.add = function(world, bodies) {
   Composite.add(world, bodies)
 }
 
-var getAllBodies = function(world) {
+// this(world)
+util_world.all = function(world) {
   return Composite.allBodies(world);
 }

d40ef61ea7b65b41ac38876fd9d65adcf35f0326
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-08 12:59:12 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,6 +80,52 @@ var init = function() {
       showMousePosition: true, // to display the mouse coords, see Mouse.create(canvas) above
     }
   })
+  
+  // MOUSE TEST
+  
+  // keep track of current bounds scale (view zoom)
+  var boundsScaleTarget = 1,
+      boundsScale = {
+        x: 1,
+        y: 1
+      };
+  
+  // render event listener
+  events.beforeRender(render, function() {
+    var mouse = render.mouse,
+        world = render.engine.world,
+        translate
+    
+    var scaleFactor = mouse.wheelDelta * -0.1;
+    if (scaleFactor !== 0) {
+      if ((scaleFactor < 0 && boundsScale.x >= 0.6) || (scaleFactor > 0 && boundsScale.x <= 1.4)) {
+        boundsScaleTarget += scaleFactor;
+      }
+    }
+    
+    if (Math.abs(boundsScale.x - boundsScaleTarget) > 0.01) {
+      // smoothly tween scale factor
+      scaleFactor = (boundsScaleTarget - boundsScale.x) * 0.2;
+      boundsScale.x += scaleFactor;
+      boundsScale.y += scaleFactor;
+
+      // scale the render bounds
+      render.bounds.max.x = render.bounds.min.x + render.options.width * boundsScale.x;
+      render.bounds.max.y = render.bounds.min.y + render.options.height * boundsScale.y;
+
+      // translate so zoom is from centre of view
+      translate = {
+        x: render.options.width * scaleFactor * -0.5,
+        y: render.options.height * scaleFactor * -0.5
+      };
+
+      Bounds.translate(render.bounds, translate);
+
+      // update mouse
+      Mouse.setScale(mouse, boundsScale);
+      Mouse.setOffset(mouse, render.bounds.min);
+    }
+  })
 
   // run the renderer
   Render.run(renderer)

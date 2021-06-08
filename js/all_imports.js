0564b34fa14e28031327309b611afbdae15f98ba
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-08 12:37:00 +0800

    Create all_imports.js

---
+++ js/all_imports.js
@@ -0,0 +1,10 @@
+// ALL IMPORTS (don't know if this will cause performance issues)
+
+// util
+import { events } from "./util/events.js"
+import { util_world } from "./util/world.js"
+import { display } from "./util/display.js"
+import { style } from "./util/style.js"
+
+// config
+import { config, category } from "./config/config.js"

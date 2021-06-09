6c0cc47ec08b22cf95835fa8ba541ad6a71baa74
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-09 13:21:21 +0800

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -1,9 +1,12 @@
 export var config = {
+  version: "0.0.0",
 }
 
 export var category = {
-  you: 0x0001,
-  bad: 0x0002,
+  obstacles: 0x0001,
+  you: 0x0002,
+  bad: 0x0004,
+  mouse: 0x0008,
 }
 
 config.category = category

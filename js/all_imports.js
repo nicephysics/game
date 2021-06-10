e27152eb0ab0307c7d3d59ced6f3163270f2a5ea
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-10 10:01:32 +0800

    Update all_imports.js

--- js/all_imports.js
+++ js/all_imports.js
@@ -5,9 +5,9 @@ import { events } from "./util/events.js"
 import { math } from "./util/math.js"
 import { random } from "./util/random.js"
 import { util_world } from "./util/world.js"
-import { style } from "./util/style.js"
 
 // display
+import { style } from "./display/style.js"
 import { display } from "./display/display.js"
 import { display_view } from "./display/view.js"
 

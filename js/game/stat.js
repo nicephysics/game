fdba385c3a226d0f3b312b4d48d538579e0aeb9b
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-10 11:14:44 +0800

    Create stat.js

---
+++ js/game/stat.js
@@ -0,0 +1,99 @@
+import { category, config } from "../config/config.js"
+
+export var stats = {
+  base: { // all stats that are 1 can be ignored in the definition
+    size: 1, // [z] size of projectile, the higher the better
+    mass: 1, // [m] mass of projectile, the higher the better
+    speed: 1, // [s] speed of projectile, the higher the better
+    reload: 1, // [r] reload of the gun, the higher the better
+    inertia: 1, // [i] gravity scale (sort of), the higher the better
+    timeScale: 1, // [ts] slow motion, ???
+    restitution: 1, // [e] coefficient of restitution of the projectile, the higher the better
+    kineticFriction: 1, // [kf] kinetic friction of the projectile
+    staticFriction: 1, // [sf] static friction of the projectile
+    airResistance: 1, // [a]
+    type: "base", // [t] type of the projectile
+    other: { // [o] other options, if type !== "base"
+      // object
+    },
+  },
+  shooter: {
+    z: 10, m: 10, s: 10, r: 1, i: 1, ts: 1, e: 0.4, kf: 0, sf: 0, a: 0.001, t: "base", o: { }
+  },
+  basic: {
+    z: 0.7, m: 1, s: 1, r: 1, i: 1, ts: 1, e: 1, kf: 1, sf: 1, a: 1,
+  },
+}
+
+export class Stat {
+  // STATic
+  static stats = stats // just another reference (when only importing Stat)
+  
+  // fields
+  size = 1
+  mass = 1
+  speed = 1
+  reload = 1
+  inertia = 1
+  timeScale = 1
+  restitution = 1
+  kineticFriction = 1
+  staticFriction = 1
+  airResistance = 1
+  
+  // constructor
+  constructor(gun, stat = []) {
+    // does a set_base(stats.base) first
+    this.set(stat)
+  }
+  
+  // get
+  get array() {
+    return [
+      this.size, this.mass, this.speed, this.reload, this.inertia, this.timeScale, this.restitution, this.kineticFriction, this.staticFriction, this.airResistance
+    ]
+  }
+  
+  // set
+  
+  // go!
+  set(stat) {
+    if (stat instanceof Array) {
+      this.set_base(stats.base)
+      for (let s in stat) {
+        this.add(s)
+      }
+    } else {
+      this.set_base(stats.base)
+      this.add(stat)
+    }
+  }
+  
+  set_base(s) {
+    this.size = s.z || s.size || 1
+    this.mass = s.m || s.mass || 1
+    this.speed = s.s || s.speed || 1
+    this.reload = s.r || s.reload || 1
+    this.inertia = s.i || s.inertia || 1
+    this.timeScale = s.ts || s.timeScale || 1
+    this.restitution = s.e || s.restitution || 1
+    this.kineticFriction = s.kf || s.kineticFriction || 1
+    this.staticFriction = s.sf || s.staticFriction || 1
+    this.airResistance = s.a || s.airResistance || 1
+  }
+
+  add(s) {
+    this.type = s.t || s.type || "base"
+    
+    this.size *= s.z || s.size || 1
+    this.mass *= s.m || s.mass || 1
+    this.speed *= s.s || s.speed || 1
+    this.reload *= s.r || s.reload || 1
+    this.inertia *= s.i || s.inertia || 1
+    this.timeScale *= s.ts || s.timeScale || 1
+    this.restitution *= s.e || s.restitution || 1
+    this.kineticFriction *= s.kf || s.kineticFriction || 1
+    this.staticFriction *= s.sf || s.staticFriction || 1
+    this.airResistance *= s.a || s.airResistance || 1
+  }
+}

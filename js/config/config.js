e82ebc64975f7b24b5e89f8d7327508c7809d8df
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-10 11:39:47 +0800

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -2,12 +2,40 @@ export var config = {
   version: "0.0.0",
 }
 
-export var category = {
+var c = {
   wall: 0x0001,
   you: 0x0002,
   bad: 0x0004,
   mouse: 0x0008,
-  all: 0x0015, // 4 category types so far...
+  bullet: 0x0016,
+  all: 0x0031, // 5 category types so far, so category.all = (2^5 - 1)
+}
+
+export var category = {
+  c: c,
+  mouseConstraint: {
+    mask: c.mouse,
+  },
+  ground: {
+    category: c.wall,
+    mask: c.bad
+  },
+  yourTower: {
+    category: c.you | c.wall,
+    mask: c.bad
+  },
+  yourBullet: {
+    category: c.you | c.bullet,
+    mask: c.bad | c.wall | c.bullet
+  },
+  badTower: {
+    category: c.bad | c.wall,
+    mask: c.bad
+  },
+  badBullet: {
+    category: c.bad | c.bullet,
+    mask: c.you | c.wall | c.bullet
+  },
 }
 
 config.category = category

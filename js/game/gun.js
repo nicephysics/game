3ac0c1423ee157ca127f4a20c6f16e37b2cdddb5
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-10 11:59:17 +0800

    Update gun.js

--- js/game/gun.js
+++ js/game/gun.js
@@ -1,12 +1,24 @@
 // imports
+import { stats, Stat } from "./stat.js"
+import { category, config } from "../config/config.js"
+import { style } from "../display/display.js"
+import { Tower } from "./tower.js"
+// drawing?
 
 if (true) {
   // 2 spaces
 }
 
-var Mouse = Matter.Mouse,
+var Body = Matter.Body,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite,
+    Composites = Matter.Composites,
+    Mouse = Matter.Mouse,
     MouseConstraint = Matter.MouseConstraint,
-    Vector = Matter.Vector
+    Vector = Matter.Vector,
+    engine = Tower.engine,
+    world = Tower.world,
+    render = Tower.render
 
 export var guns = [ ] // Gun[]
 
@@ -21,53 +33,128 @@ export class Gun {
   // fields
   id = Gun._count++
   label = "Gun #" + this.id
+  bulletcount = 1
   tower = null
   position = Vector.create(0, 0)
   size = Vector.create(0, 0)
-  direction = 0
+  angle = 0
   shape = "rectangle"
-  aspects = {}
+  aspects = { } // optional
+  stat = new Stat(this)
+  children = [ ] // Matter.Body[]
+  shot = 0 // counter
   
   // constructor
-  constructor(tower, set) {
+  constructor(tower, location, stat) {
     guns.push(this)
     
     this.tower = tower
-    this.setLocation(set)
+    this.setStat(stat)
+    this.setLocation(location)
   }
   
   // get
+  get location() {
+    return Vector.add(this.tower.position, Vector.mult(this.position, this.tower.size * Gun.set.scale))
+  }
+  get x() {
+    return this.location.x
+  }
+  get y() {
+    return this.location.y
+  }
+  get width() {
+    return this.size.x * this.tower.size * Gun.set.scale
+  }
+  get height() {
+    return this.size.y * this.tower.size * Gun.set.scale
+  }
+  get direction() {
+    return this.angle + this.tower.direction
+  }
+  get gunDifference() {
+    return Vector.create(
+      this.height * Math.sin(this.direction),
+      this.height * Math.cos(this.direction)
+    )
+  }
+  get gunEnd() {
+    return Vector.add(this.location, this.gunDifference)
+  }
   
   // set
   
   // go!
   draw(render) {
-    // very huge todo
+    var ctx = render.context
     switch (this.shape) {
       case "rectangle":
+        // todo
+        break;
+      case "circle": // a CIRCULAR gun???
+        // todo
         break;
     }
+  }
+  
+  tick() {
+    this.shot++
+    if (this.tower.controlled) {
+      // hmmm
+    } else {
+      if (this.shot > this.stat.reloadFrames) {
+        this.shot = 0
+        this.shoot()
+      }
+    }
+  }
+  
+  shoot() {
+    var s = this.stat
+    var b = Bodies.circle(this.gunEnd.x, this.gunEnd.y, this.width * s.size, {
+      isStatic: false,
+      label: "Bullet #" + (this.bulletcount++) + " from " + this.label,
+      categoryFilter: category.yourBullet,
+      render: style.tower.basic, // todo
+      density: s.mass,
+      friction: s.kineticFriction,
+      frictionStatic: s.staticFriction,
+      frictionAir: s.airResistance,
+      velocity: Vector.mult(this.direction, s.speed),
+    })
+    // b.setInertia(b.inertia * s.inertia) // ?
+    Composite.add(world, b)
+    this.children.push(b)
+  }
+  
+  setStat(s) {
+    this.stat.set(s)
   }
   
   setLocation(set) {
     this.position = Vector.create(set.x, set.y)
     this.size = Vector.create(set.w, set.h)
-    this.direction = set.d || 0
+    this.angle = set.a || 0
     this.shape = set.s || "rectangle"
     this.aspects = set.aspects || { }
   }
 }
 
+Gun.set = {
+  // overall gun scale
+  scale = 0.1,
+}
+
 Gun.set.some_random_comments = {
-  x: 0, // position.x
-  y: 0, // position.y
-  w: 20, // size.x
-  h: 50, // size.y
-  d: 0, // direction (default: 0)
+  x: 0, // position.x (*)
+  y: 0, // position.y (*)
+  w: 0, // size.x (*)
+  h: 50, // size.y (*)
+  a: 0, // angle (default: 0)
   s: "", // shape (default: rectangle)
   aspects: { } // shape aspects (default: nothing)
 }
 
 Gun.set.default = {
-  x: 0, y: 0, w: 20, h: 50, d: 0, s: "rectangle",
+  x: 0, y: 0, w: 0, h: 50, a: 0, s: "rectangle",
 }

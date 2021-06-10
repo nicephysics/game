0423a7a464e717e35a49d5110a8dd8dfb5e39daa
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-10 10:00:50 +0800

    Update and rename js/util/style.js to js/display/style.js

---
+++ js/display/style.js
@@ -0,0 +1,41 @@
+/* this file creates render objects */
+
+export var style = { }
+
+style.default = {
+  ground: style.fill("#bd3900"),
+}
+
+style.tower = {
+  basic: style.fill("#009ebd"),
+}
+
+style.fill = function(fillStyle, opacity = 1) {
+  return {
+    fillStyle: fillStyle,
+    strokeStyle: "#000000",
+    lineWidth: 1,
+    opacity: opacity,
+  }
+}
+
+style.stroke = function(strokeStyle, lineWidth = 1, opacity = 1) {
+  return {
+    fillStyle: "#00000000",
+    strokeStyle: strokeStyle,
+    lineWidth: lineWidth,
+    opacity: opacity,
+  }
+}
+
+style.sprite = function(texturePath, xOffset = 0.5, yOffset = 0.5, xScale = 1.0, yScale = 1.0) {
+  return {
+    sprite: {
+      texture: texturePath,
+      xOffset: xOffset,
+      yOffset: yOffset,
+      xScale: xScale,
+      yScale: yScale,
+    }
+  }
+}

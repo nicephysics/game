d9a8995ec12314d7f5e82f36effa7791c6603cad
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-10 13:20:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -107,6 +107,11 @@ var init = function() {
   Tower.init(render)
   var tower = new Tower()
   tower.test_1()
+  
+  events.afterRender(render, function() {
+    Tower.tickAll()
+    Tower.drawAll()
+  })
 
   // run the renderer
   Render.run(render)

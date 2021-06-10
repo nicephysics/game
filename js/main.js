ec795b2c62e78bdde3faf0b0907102346751256f
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-10 18:14:41 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -118,7 +118,7 @@ var init = function() {
     for (let body of all) {
       let gravityScale = body.gravityScale
       if (gravityScale == null) continue;
-      let scale = scale - 1
+      let scale = gravityScale - 1
       Body.applyForce(body, body.position, {
         x: gravity.x * scale * gravity.scale * body.mass,
         y: gravity.y * scale * gravity.scale * body.mass

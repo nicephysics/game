30993712956cf00876f69e2c583bf0abb7b2e884
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-11 16:27:59 +0800

    Update all_imports.js

--- js/all_imports.js
+++ js/all_imports.js
@@ -25,10 +25,64 @@ import { towerstats, TowerStat } from "./game/towerstat.js"
 */
 
 // hack for console
-var a, e, g, s, t, ts
-import('./game/aim.js').then(m => a = m)
-import('./game/enemy.js').then(m => e = m)
-import('./game/gun.js').then(m => g = m)
-import('./game/stat.js').then(m => s = m)
-import('./game/tower.js').then(m => t = m)
-import('./game/towerstat.js').then(m => ts = m)
+
+// game
+var game = { }
+import('./game/aim.js').then(m => {
+  game.aim = m.aim
+})
+import('./game/enemy.js').then(m => {
+  game.Enemy = m.Enemy
+})
+import('./game/gun.js').then(m => {
+  game.Gun = m.Gun
+})
+import('./game/stat.js').then(m => {
+  game.Stat = m.Stat
+  game.stats = m.stats
+})
+import('./game/tower.js').then(m => {
+  game.Tower = m.Tower
+  game.towers = m.towers
+})
+import('./game/towerstat.js').then(m => {
+  game.TowerStat = m.TowerStat
+  game.towerstats = m.towerstats
+})
+
+// display
+var disp = { }
+import('./display/draw.js').then(m => {
+  disp.draw = m.draw
+})
+import('./display/view.js').then(m => {
+  disp.display_view = m.display_view
+})
+import('./display/style.js').then(m => {
+  disp.style = m.style
+})
+import('./display/display.js').then(m => {
+  disp.display = m.display
+})
+
+// config
+var config = { }
+import('./config/config.js').then(m => {
+  config.config = m.config
+  config.category = m.category
+})
+
+// util
+var util = { }
+import('./util/events.js').then(m => {
+  util.events = m.events
+})
+import('./util/math.js').then(m => {
+  util.math = m.math
+})
+import('./util/random.js').then(m => {
+  util.random = m.random
+})
+import('./util/world.js').then(m => {
+  util.world = m.world
+})

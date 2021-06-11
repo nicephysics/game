dbcf3e68e4ef1e10982a9967f054648c9c8a1f02
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-11 16:51:56 +0800

    Update aim.js

--- js/game/aim.js
+++ js/game/aim.js
@@ -5,6 +5,7 @@ export var aim = {
 var Vector = Matter.Vector
 
 // aim! credits: https://math.stackexchange.com/questions/1792507/finding-launch-angle-for-two-projectile-collision
+// IT WORKS
 aim.aim = function(position, velocity, targetPosition, targetVelocity, targetTime, gravity) {
   function s(a) { // square
     return a * a
@@ -45,7 +46,6 @@ aim.aim = function(position, velocity, targetPosition, targetVelocity, targetTim
   //var d = 2*(sg*st-2*v*g*t*sin+sv+su),
   //    q = v*(3*g*st-2*y)*sin+2*v*x*cos-t*(sg*st-2*g*y+2*v),
   //    s = st*(sg*st*su-4*sg*sx-4*g*su*y+4*su*sv)+4*(su*sx+su*sy-sv*sx) + 4*t*v*x*(sg*st+2*g*y-2*su)*cos-sv*(g*st*2*x+2*y)*(g*st-2*x+2*y)*s(cos) - 4*sv*x*(g*st+2*y)*cos*sin-4*v*t(g*st*su-2*g*sx-2*su*y)*sin
-      
   var s = t*t*(g*g*t*t*u*u - 4*g*g*x*x - 4*g*u*u*y+4*u*u*v*v)+4*u*u*x*x+4*u*u*y*y-4*v*v*x*x + cos*4*t*v*x*(g*g*t*t+2*g*y-2*u*u) - cos*cos*v*v*(g*t*t+2*x+2*y)*(g*t*t-2*x+2*y) - cos*sin*4*v*v*x*(g*t*t+2*y) - sin*4*v*t*(g*t*t*u*u-2*g*x*x-2*u*u*y),
       d = 2*(g*g*t*t - 2*v*sin*g*t - u*u + v*v),
       q = sin*v*(3*g*t*t - 2*y) + 2*cos*v*x - t*(g*g*t*t - 2*g*y + 2*v*v)
@@ -65,9 +65,10 @@ aim.aim = function(position, velocity, targetPosition, targetVelocity, targetTim
   var c = (x-cos*v*(t+ti))/(ti*u)
   if (c <= 0.0 || c >= 1.0) return console.log("Incorrect launch angle:", c)
   var ta = atan2(sr(1-c*c), c)
-  console.log(ti, a)
-  return a
+  console.log(ti, c)
+  return c
 }
 
-aim.aim(Vector.create(0, 0), 120, Vector.create(1000, 30), Vector.mult(Vector.create(-Math.cos(Math.PI/6), Math.sin(Math.PI/6)), 100), 5, 9.81)
+// should return 2.9310744713341257 0.5235987755982987
+console.log(0.5235987755982987 === aim.aim(Vector.create(0, 0), 120, Vector.create(1000, 30), Vector.mult(Vector.create(-Math.cos(Math.PI/6), Math.sin(Math.PI/6)), 100), 5, 9.81))
 

fd326094c143c11bfc012c82b25e834369d7b4b4
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-11 16:18:24 +0800

    Create enemy.js

---
+++ js/game/enemy.js
@@ -0,0 +1,56 @@
+import { category, config } from "../config/config.js"
+import { style } from "../display/style.js"
+import { draw } from "../display/draw.js"
+
+export var enemystats = { }
+export var enemies = [ ]
+
+export class Enemy {
+  // static
+  static enemies = enemies
+  static create() {
+    
+  }
+  static wave(str, num) {
+    
+  }
+  
+  // fields
+  body = null // Matter.Body
+  // constructor
+  constructor() {
+    
+  }
+  
+  // get
+  get position() {
+    return this.body.position
+  }
+  
+  // set
+  
+  // go!
+  tick() {
+    
+  }
+  
+  draw() {
+    
+  }
+  
+  refresh() {
+    
+  }
+  
+  createBody() {
+    
+  }
+  
+  removeBody() {
+    
+  }
+  
+  contemplationOfMortality() {
+    
+  }
+}

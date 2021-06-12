9e9a016bdf2bf4f3139fa226e074a33b69b74a0a
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-12 18:01:24 +0800

    Update aim.js

--- js/game/aim.js
+++ js/game/aim.js
@@ -39,19 +39,19 @@ aim.aim = function(position, velocity, targetPosition, targetVelocity, targetTim
       atan2 = Math.atan2,
       zzz = s(sin)*sv+2*g*y
   console.log(v, a)
-  if (zzz < 0) return console.log("Target projectile never reaches the ground!", zzz)
+  if (zzz < 0) return console.err("Target projectile never reaches the ground!", zzz)
   var tz = sin*v/g+sr(zzz)/g-t
-  if (tz < 0) return console.log("Target projectile already reached destination", -tz, "seconds before launch.")
+  if (tz < 0) return console.err("Target projectile already reached destination", -tz, "seconds before launch.")
   var tmin = max(0,(x-cos*t*v)/(cos*v+u)),
       tmax = min(tz,(x-cos*t*v)/(cos*v))
-  if (tmin > tmax) return console.log("No suitable launch time!", tmin, tmax)
+  if (tmin > tmax) return console.err("No suitable launch time!", tmin, tmax)
   //var d = 2*(sg*st-2*v*g*t*sin+sv+su),
   //    q = v*(3*g*st-2*y)*sin+2*v*x*cos-t*(sg*st-2*g*y+2*v),
   //    s = st*(sg*st*su-4*sg*sx-4*g*su*y+4*su*sv)+4*(su*sx+su*sy-sv*sx) + 4*t*v*x*(sg*st+2*g*y-2*su)*cos-sv*(g*st*2*x+2*y)*(g*st-2*x+2*y)*s(cos) - 4*sv*x*(g*st+2*y)*cos*sin-4*v*t(g*st*su-2*g*sx-2*su*y)*sin
   var s = t*t*(g*g*t*t*u*u - 4*g*g*x*x - 4*g*u*u*y+4*u*u*v*v)+4*u*u*x*x+4*u*u*y*y-4*v*v*x*x + cos*4*t*v*x*(g*g*t*t+2*g*y-2*u*u) - cos*cos*v*v*(g*t*t+2*x+2*y)*(g*t*t-2*x+2*y) - cos*sin*4*v*v*x*(g*t*t+2*y) - sin*4*v*t*(g*t*t*u*u-2*g*x*x-2*u*u*y),
       d = 2*(g*g*t*t - 2*v*sin*g*t - u*u + v*v),
       q = sin*v*(3*g*t*t - 2*y) + 2*cos*v*x - t*(g*g*t*t - 2*g*y + 2*v*v)
-  if (s < 0 || d === 0) return console.log("Launch is not possible!", s, d)
+  if (s < 0 || d === 0) return console.err("Launch is not possible!", s, d)
   var t1 = (q + sr(s)) / d,
       t2 = (q - sr(s)) / d,
       ti
@@ -62,12 +62,12 @@ aim.aim = function(position, velocity, targetPosition, targetVelocity, targetTim
   } else if (t2 >= tmin && t2 <= tmax) {
     ti = t2
   } else {
-    return console.log("Launch will fail!")
+    return console.err("Launch will fail!")
   }
   var c = (x-cos*v*(t+ti))/(ti*u)
-  if (c <= 0.0 || c >= 1.0) return console.log("Incorrect launch angle:", c)
+  if (c <= 0.0 || c >= 1.0) return console.err("Incorrect launch angle:", c)
   var ta = atan2(sr(1-c*c), c)
-  console.log(ti, ta)
+  console.err(ti, ta)
   return ta
 }
 

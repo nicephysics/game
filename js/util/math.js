a2073c00e258df496be14d61cecd911f4232fe99
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-13 08:54:50 +0800

    Update math.js

--- js/util/math.js
+++ js/util/math.js
@@ -1,7 +1,21 @@
 
+if (true) {
+  // 2 space indent!
+}
+
+var Query = Matter.Query,
+    Vector = Matter.Vector
+
 export var math = { }
 
 // e.g. lerp(start, end, 0) == start and lerp(start, end, 1) == end
 math.lerp = function(start, end, factor) {
   return start * (1 - factor) + end * factor
+}
+
+math.lerpV = function(start, end, factor) {
+  return Vector.create(
+    math.lerp(start.x, end.x, factor),
+    math.lerp(start.y, end.y, factor)
+  )
 }

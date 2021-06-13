2cb0f518e155e6e5efce5768b27c2ef93188ad69
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-13 09:07:48 +0800

    Update controls.js

--- js/game/controls.js
+++ js/game/controls.js
@@ -18,28 +18,91 @@ controls.init = function(engine) {
       player = Tower.player,
       body = player.body
   
-  function move(x, y) {
-    player.moveBy(x, y)
+  var direction = Vector.create(0, 0)
+  
+  player.control = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+    shoot: false,
+    altshoot: false,
+    midshoot: false,
+    autoshoot: false,
+    autorotate: false,
   }
+  
+  var c = player.control // alias
   
   document.addEventListener("keydown", function(event) {
     switch (event.code) {
       case "KeyS":
       case "ArrowDown":
-        move(0, 1)
-        break;
+        c.down = true
+        break
       case "KeyW":
       case "ArrowUp":
-        move(0, -1)
-        break;
+        c.up = true
+        break
       case "KeyA":
       case "ArrowLeft":
-        move(-1, 0)
-        break;
+        c.left = true
+        break
       case "KeyD":
       case "ArrowRight":
-        move(1, 0)
-        break;
+        c.right = true
+        break
+    }
+  })
+  
+  document.addEventListener("keyup", function(event) {
+    switch (event.code) {
+      case "KeyS":
+      case "ArrowDown":
+        c.down = false
+        break
+      case "KeyW":
+      case "ArrowUp":
+        c.up = false
+        break
+      case "KeyA":
+      case "ArrowLeft":
+        c.left = false
+        break
+      case "KeyD":
+      case "ArrowRight":
+        c.right = false
+        break
+      case "KeyE":
+        c.autoshoot = !c.autoshoot
+        break
+      case "KeyR":
+        c.autorotate = !c.autorotate
+        break
+    }
+  })
+  
+  document.addEventListener("mousedown", function(event) {
+    if ((event.buttons & 1) > 0) {
+      c.shoot = true
+    }
+    if ((event.buttons & 2) > 0) {
+      c.altshoot = true
+    }
+    if ((event.buttons & 4) > 0) {
+      c.midshoot = true
+    }
+  })
+  
+  document.addEventListener("mouseup", function(event) {
+    if ((event.buttons & 1) > 0) {
+      c.shoot = false
+    }
+    if ((event.buttons & 2) > 0) {
+      c.altshoot = false
+    }
+    if ((event.buttons & 4) > 0) {
+      c.midshoot = false
     }
   })
 }

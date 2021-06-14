d7e460f9dba3d7ffd8e13c1eb798a3268dd8a605
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-14 19:10:58 +0800

    Create ui.js

---
+++ js/display/ui.js
@@ -0,0 +1,11 @@
+import { draw } from "./draw.js"
+import { style } from "./style.js"
+
+import { Tower } from "../game/tower.js"
+
+export var ui = { }
+
+ui.draw = function() {
+  var render = Tower.render,
+      ctx = render.context
+}

3a10a37cb47d0d0732a845074528723afc02b7cb
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-14 11:20:25 +0800

    Create gunset.js

---
+++ js/game/gunset.js
@@ -0,0 +1,43 @@
+
+export var gunset = {
+  // overall gun scale
+  scale: 0.1,
+}
+
+gunset.some_random_comments = {
+  x: 0, // position.x (*)
+  y: 0, // position.y (*)
+  w: 0, // size.x (*)
+  h: 10, // size.y (*)
+  a: 0, // angle (default: 0)
+  d: 0, // shooting delay (default: 0)
+  style: "#a7a7af", // fill style (default: "#a7a7af")
+  stroke: null, // stroke style (default: same as fill style)
+  lineWidth: 3, // stroke line width (default: 3)
+  shape: "rectangle", // shape (default: rectangle)
+  dummy: false, // dummy gun (default: false)
+  aspects: { } // shape aspects (default: nothing)
+}
+
+gunset.default = {
+  x: 0, y: 0, w: 0, h: 10, a: 0, d: 0,
+  shape: "rectangle",
+}
+
+gunset.basic = {
+  x: 0, y: 0, w: 0, h: 10, a: 0, d: 0,
+  style: "basic",
+  stat: ["shooter", "basic"],
+}
+
+gunset.double_left = {
+  x: -0.54, y: 0, w: 0, h: 10, a: 0, d: 0,
+  style: "double",
+  stat: ["shooter", "double"],
+}
+
+gunset.double_right = {
+  x: 0.54, y: 0, w: 0, h: 10, a: 0, d: 0.5,
+  style: "double",
+  stat: ["shooter", "double"],
+}

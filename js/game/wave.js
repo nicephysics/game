756c2da2acbcae3765a6a692a3c8ae8c798e20db
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-14 22:48:58 +0800

    Update wave.js

--- js/game/wave.js
+++ js/game/wave.js
@@ -1,7 +1,11 @@
 import { category, config } from "../config/config.js"
+
 import { Enemy } from "./enemy.js"
+
 import { draw } from "../display/draw.js"
+
 import { random } from "../util/random.js"
+import { math } from "../util/math.js"
 
 export var wave = { }
 
@@ -50,10 +54,59 @@ wave.make = function(types, seed, options = { }) {
   return w // result
 }
 
-wave.repeat = function(type, options = { }) {
-  
+wave.get = function(type, number, sep, difficulty, options = { }) {
+  return {
+    type: type,
+    number: number,
+    sep: sep,
+    difficulty: difficulty,
+    options = { },
+  }
 }
 
-wave.get = function(type, number, sep, difficulty) {
+// repeats a wave many times with changing number/difficulty (note: o stands for options)
+/* possible options: {
+  mult: {
+    number: 1,
+    difficulty: 1,
+    sep: 1,
+  },
+  add: {
+    number: 0,
+    difficulty: 0,
+  }
+  septypes: [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.8, 2.0],
+  random: false,
+  seed: null, // random seed
+  change: false, // do the values change?
+  start: 1, // starting wave
+} */
+wave.repeat = function(types, number, sep, difficulty, rep, o = { }) {
+  var types = (types instanceof Array) ? types : [types],
+      mult = o.mult || { },
+      add = o.add || { },
+      septypes = o.septypes || [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.8, 2.0],
+      isrand = o.random || false,
+      gen = (isrand) ? random.fun(o.seed || 1234567890) : function() { console.log("Why is the gen() function in wave.repeat called?") },
+      ischange = o.change || true,
+      ans = [ ]
   
+  for (let i = 0; i < rep; ++i) {
+    var separation = (isrand) ? septypes[Math.floor(gen() * septypes.length)] : septypes[i % septypes.length]
+    ans.push( {
+      type: (isrand) ? types[Math.floor(gen() * types.length)] : types[i % types.length],
+      number: number / Math.pow(separation, 0.5),
+      sep: sep * separation,
+      difficulty: difficulty * Math.pow(separation, 0.8),
+    } )
+    if (ischange /* && i + 1 < rep */) {
+      number *= mult.number || 1
+      difficulty *= difficulty.number || 1
+      sep *= sep.number || 1
+      number += add.number || 1
+      difficulty += add.difficulty || 1
+    }
+  }
+  
+  return ans
 }

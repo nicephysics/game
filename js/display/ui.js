1f932d1f1a82d1e3d67b8c6b44c08d889d6cb511
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-16 18:11:12 +0800

    Update ui.js

--- js/display/ui.js
+++ js/display/ui.js
@@ -11,8 +11,9 @@ export var ui = { }
 ui.vars = {
   // constants
   
-  xp_bar_length: 300,
+  xp_bar_length_ratio: 0.50, // ratio of height
   xp_bar_side_x: 30,
+  xp_bar_show: 1,
   xp_bar_side_x_mouse: 60,
   xp_bar_color: "#ff801f",
   xp_bar_lerp: 0.05,
@@ -37,46 +38,54 @@ ui.draw = function() {
       playerX = player.x,
       playerY = player.y,
       _width = render.options.width,
-      _height = render.options.height
+      _height = render.options.height,
+      x, y // define x and y first
   
   // bar var
-  var color = v.xp_bar_color,
+  var xp_show = v.xp_bar_show,
       xp = math.lerp(v.xp_bar_xp, player.xp, v.xp_bar_lerp),
-      smoothing = ( Math.abs(player.xp - v.xp_bar_xp) >= v.xp_text_appear_amount ), // whether the UI is currently still smoothing
-      level = math.towerlevel(xp),
-      current = xp - math.towerxp(level),
-      next = math.towerxpneeded(level),
-      ratio = current / next,
-      rBall = v.xp_ball_radius,
-      x = _width - v.xp_bar_side_x,
-      y = 0, // unused for bar var
-      y1 = _height / 2 - 150 - rBall * 2,
-      y2 = y1 + 300,
-      mid = y2 - ratio * 300,
-      yBall = y2 + rBall
-  v.xp_bar_xp = xp
-  // draw! (remember to add ctx)
-  ctx.lineCap = 'round'
-  draw.setFill(ctx, "transparent")
-  draw.setLineWidth(ctx, 10)
-  draw.setDarkStroke(ctx, color)
-  draw._line(ctx, x, y1, x, y2)
-  draw.setLineWidth(ctx, 8)
-  draw.setLightStroke(ctx, color)
-  draw._line(ctx, x, y1, x, y2)
-  draw.setStroke(ctx, color)
-  draw._line(ctx, x, mid, x, y2)
-  // draw ball!
-  draw.setDarkFill(ctx, color)
-  draw.setStroke(ctx, "transparent")
-  draw.setFont(ctx, Math.floor(v.xp_ball_font_size) + "px Roboto Condensed")
-  draw._circle(ctx, x, yBall - 2, rBall)
-  draw.setLightFill(ctx, color)
-  draw._text(ctx, x, yBall, level + "", "center")
-  // check mouse!
-  if (smoothing || ( mousepos.x > (_width - v.xp_bar_side_x_mouse) && mousepos.y > y1 && mousepos.y < yBall )) {
-    draw.setFont(ctx, Math.floor(v.xp_text_font_size) + "px Roboto Condensed")
+      smoothing = ( Math.abs(player.xp - v.xp_bar_xp) >= v.xp_text_appear_amount ) // whether the UI is currently still smoothing
+  if (xp_show > 0 || smoothing) {
+    var color = v.xp_bar_color,
+        level = math.towerlevel(xp),
+        current = xp - math.towerxp(level),
+        next = math.towerxpneeded(level),
+        ratio = current / next,
+        rBall = v.xp_ball_radius,
+        x = _width - v.xp_bar_side_x * xp_show,
+        height = v.xp_bar_length_ratio * _height,
+        y1 = _height / 2 - height / 2 - rBall * 2,
+        y2 = y1 + height,
+        mid = y2 - ratio * height,
+        yBall = y2 + rBall
+    v.xp_bar_xp = xp
+    // draw! (remember to add ctx)
+    ctx.lineCap = 'round'
+    draw.setFill(ctx, "transparent")
+    draw.setLineWidth(ctx, 10)
+    draw.setDarkStroke(ctx, color)
+    draw._line(ctx, x, y1, x, y2)
+    draw.setLineWidth(ctx, 8)
+    draw.setLightStroke(ctx, color)
+    draw._line(ctx, x, y1, x, y2)
+    draw.setStroke(ctx, color)
+    draw._line(ctx, x, mid, x, y2)
+    // draw ball!
     draw.setDarkFill(ctx, color)
-    draw._text(ctx, x - 15, mid, Math.round(current) + "/" + Math.round(next), "right")
+    draw.setStroke(ctx, "transparent")
+    draw.setFont(ctx, Math.floor(v.xp_ball_font_size) + "px Roboto Condensed")
+    draw._circle(ctx, x, yBall - 2, rBall)
+    draw.setLightFill(ctx, color)
+    draw._text(ctx, x, yBall, level + "", "center")
+    // check mouse!
+    if (smoothing || ( mousepos.x > (_width - v.xp_bar_side_x_mouse) && mousepos.y > y1 && mousepos.y < yBall )) {
+      draw.setFont(ctx, Math.floor(v.xp_text_font_size) + "px Roboto Condensed")
+      draw.setDarkFill(ctx, color)
+      draw._text(ctx, x - 15, mid, Math.round(current) + "/" + Math.round(next), "right")
+    }
   }
+  
+  // upgrade buttons
+  // upgrade side
+  // upgrade overlay
 }

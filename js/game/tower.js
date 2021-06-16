36bdfcc81a484143826f34bf846818d6620e768c
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-16 10:03:53 +0800

    Update tower.js

--- js/game/tower.js
+++ js/game/tower.js
@@ -195,7 +195,7 @@ export class Tower {
     if (c.left) movedir.x--
     if (c.right) movedir.x++
     // move variables
-    var speed = this.stat.speed * this.effect.speedmult,
+    var speed = this.stat.speed * (this.effect.speedmult || 1),
         moveVector = Vector.mult(Vector.normalise(movedir), speed),
         moveResult = Vector.add(this.targetpos, moveVector)
     // move limits

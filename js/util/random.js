75bac885805522f38fc08234618a7464bbe2c413
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-16 12:11:10 +0800

    Update random.js

--- js/util/random.js
+++ js/util/random.js
@@ -69,13 +69,13 @@ random.fun = function(seed) {
 
 
 random.gauss = function(mean, deviation) {
-    let x1, x2, w;
-    do {
-        x1 = 2 * Math.random() - 1;
-        x2 = 2 * Math.random() - 1;
-        w = x1 * x1 + x2 * x2;
-    } while (0 == w || w >= 1);
+  let x1, x2, w;
+  do {
+    x1 = 2 * Math.random() - 1;
+    x2 = 2 * Math.random() - 1;
+    w = x1 * x1 + x2 * x2;
+  } while (0 == w || w >= 1);
 
-    w = Math.sqrt(-2 * Math.log(w) / w);
-    return mean + deviation * x1 * w;
+  w = Math.sqrt(-2 * Math.log(w) / w);
+  return mean + deviation * x1 * w;
 }

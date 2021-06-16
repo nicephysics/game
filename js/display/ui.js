f5651b90dc8869babfd5bff197003f25c8cd3b7c
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-16 13:04:05 +0800

    Update ui.js

--- js/display/ui.js
+++ js/display/ui.js
@@ -7,5 +7,29 @@ export var ui = { }
 
 ui.draw = function() {
   var render = Tower.render,
-      ctx = render.context
+      ctx = render.context,
+      player = Tower.player,
+      _width = render.options.width,
+      _height = render.options.height
+  
+  // bar
+  var xp = player.xp,
+      level = player.level,
+      current = xp - math.towerxp(level - 1),
+      next = math.towerxpneeded(level),
+      ratio = current / next,
+      x = _width - 20,
+      y1 = _height / 2 - 150,
+      y2 = _height / 2 + 150,
+      mid = _height / 2 + 150 - ratio * 300
+  // draw!
+  draw.setFill("transparent")
+  draw.setLineWidth(7)
+  draw.setDarkStroke("#ff801f")
+  draw._line(x, y1, x, y2)
+  draw.setLineWidth(5)
+  draw.setLightStroke("#ff801f")
+  draw._line(x, y1, x, y2)
+  draw.setStroke("#ff801f")
+  draw._line(x, y1, x, mid)
 }

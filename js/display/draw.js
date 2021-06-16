d27935a41864dc266d1e5392e818dea6440f5c32
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-16 12:51:44 +0800

    Update draw.js

--- js/display/draw.js
+++ js/display/draw.js
@@ -98,6 +98,24 @@ draw.arc = function(render, x, y, r, start, end, counter = false) {
   draw._arc(ctx, x, y, r, start, end, counter)
 }
 
+draw._line = function(ctx, x1, y1, x2, y2) {
+  ctx = ctx || draw.ctx
+  ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+  ctx.stroke()
+  ctx.fill()
+}
+
+draw.line = function(render, x1, y1, x2, y2) {
+  x1 -= render.bounds.min.x
+  y1 -= render.bounds.min.y
+  x2 -= render.bounds.min.x
+  y2 -= render.bounds.min.y
+  var ctx = render.context
+  draw._arc(ctx, x1, y1, x2, y2)
+}
+
 draw.gun = function(render, x, y, length, height, aspect, angle) {
   x -= render.bounds.min.x
   y -= render.bounds.min.y

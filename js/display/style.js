580e8904b2354b5441aaa565befcdd37eaae862c
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-20 18:09:06 +0800

    Update style.js

--- js/display/style.js
+++ js/display/style.js
@@ -46,7 +46,7 @@ style.sprite = function(texturePath, xOffset = 0.5, yOffset = 0.5, xScale = 1.0,
 style.default = {
   ground: style.fillStroke("#005fbd", 1, 0),
   wall: style.fillStroke("#bd3900", 1, 0),
-  atmosphere: style.fillStroke("#7a5bd4", 0.3, 5),
+  atmosphere: style.fillStroke("#8984e8", 0.3, 5), // #7a5bd4
   /* {
     strokeStyle: "#554fff",
     lineWidth: 5,

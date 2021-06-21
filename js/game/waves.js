fd8ffe4d8b8b185b00dc8deb87516968b52483d4
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-21 13:36:50 +0800

    Update waves.js

--- js/game/waves.js
+++ js/game/waves.js
@@ -9,10 +9,15 @@ import { wave } from "./wave.js"
 import { random } from "../util/random.js"
 import { math } from "../util/math.js"
 
+if (true) {
+  // 2 space indent!
+}
+
 const Vector = Matter.Vector,
-      Make = wave.make,
+      Make = wave.make, // use or not?
       Repeat = wave.repeat
 
+// to fill up later
 export var waves = { }
 
 waves.current = 0
@@ -21,14 +26,31 @@ waves.init = function(name) {
   waves.waves = waves[name]
 }
 
-waves.t1 = [ ]
-waves.t1.push(
+
+waves.tut1 = {
+  wave: [],
+}
+waves.tut1.wave.push(
   { // 1
-    type: 1,
+    type: "asteroid",
     number: 2,
     sep: 10,
     m: 0.1, s: 0.5, g: 0.2, // MSG!
   }, { // 2
-    
+    type: "asteroid",
+    number: 5,
+    sep: 8,
+    m: 0.1, s: 0.5, g: 0.2, // MSG!    
   }
 )
+waves.tut1.text = {
+  [1]: ["Welcome to the tutorial! You are a [thing] trying to protect the planet from incoming asteroids.",
+        "Use ↑↓→← keys to move and click (and hold) to launch a projectile.",
+        "Press the start button to begin! Asteroids will begin to appear after a while."
+        ],
+  [2]: ["Did you hit the 2 asteroids away? More of them will come this time..."],
+  [3]: ["The points you gain from hitting asteroids can be used for upgrading!",
+        "Press the statistics button in the bottom right (or press U) to enter the upgrade screen.",
+        "From there, you can press the upgrade button!",
+        ],
+}

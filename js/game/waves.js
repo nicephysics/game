731851ca71f43afccee6d46b6bf46b8726c66bf4
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-23 10:43:08 +0800

    Update waves.js

--- js/game/waves.js
+++ js/game/waves.js
@@ -94,14 +94,15 @@ waves.tut1.wave.push(
 waves.tut1.starttext = {
   [0]: ["Welcome to the tutorial! You are a [thing] trying to protect the planet from incoming asteroids.",
         "Use ↑↓→← keys to move and click (and hold) to launch a projectile.",
-        "Press the play button at the top of the screen to begin!"
+        "The lighter area of the screen is the atmosphere of the planet. Everything inside the atmosphere falls due to gravity and everything above it floats in space!"
+        "Ready? Press the play button at the top of the screen to begin!"
         ],
   [1]: ["The first asteroids should start to appear now."],
-  [4]: ["Many asteroids can come at once! Get ready!",],
+  [4]: ["Many asteroids can appear at the same time too!",],
 }
 waves.tut1.endtext = {
   [1]: ["Did you hit the 2 asteroids away? More of them will come this time..."],
-  [2]: ["The asteroids will start to come at a higher rate after this! Try to keep up!"],
+  [2]: ["The asteroids will start to come at a faster rate after this, try to keep up!"],
   [3]: ["The points you gain from hitting asteroids can be used for upgrading!",
         "Press the statistics button in the bottom right (or press U) to enter the upgrade screen.",
         "In the upgrade screen, you can improve your [thing] by pressing the plus button on any one of the improvements!",

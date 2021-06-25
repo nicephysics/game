ac36673a9cdc06d9e52c467d9789b3dfd5fee640
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-25 22:34:21 +0800

    Create thingstat.js

---
+++ js/game/thingstat.js
@@ -0,0 +1,19 @@
+import { category, config } from "../config/config.js"
+
+import { things } from "./things.js"
+
+if (true) {
+  // 2 space indent!
+}
+
+export class ThingStat {
+  // static
+  
+  // fields
+  
+  // constructor
+  constructor(thing) {
+    
+  }
+  
+}

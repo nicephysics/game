7712087270ba8864ab53b364a4f9ddf4640c054c
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-25 23:10:10 +0800

    Update things.js

--- js/game/things.js
+++ js/game/things.js
@@ -1,1 +1,67 @@
+import { category, config } from "../config/config.js"
 
+import { style } from "../display/style.js"
+
+if (true) {
+  // 2 space indent!
+}
+
+export var things = { }
+
+// default tower and enemy
+
+things.tower = {
+  parent: ["thing"],
+  gametype: "tower",
+  // matter stuff
+  label: "Unknown Tower",
+  static: true,
+  category: category.yourTower,
+  // render stuff
+  shape: "circle",
+  accessories: [],
+  render: style.tower.default,
+  // game stuff
+  guns: [],
+  controlType: "player",
+  
+}
+
+things.enemy = {
+  parent: ["thing"],
+  gametype: "enemy",
+  // matter stuff
+  label: "Unknown Enemy",
+  static: false,
+  category: category.enemy,
+  // render stuff
+  shape: "asteroid",
+  accessories: [],
+  render: style.enemy.default,
+  // game stuff
+  guns: [],
+  controlType: "enemy",
+  
+}
+
+
+// TOWERS
+
+things.basic = {
+  parent: ["tower"],
+  label: "Basic Tower",
+  render: style.tower.basic,
+  guns: [
+    {
+      set: {
+        x: 0, y: 0, w: 0, h: 10, a: 0, d: 0,
+        style: "basic",
+      },
+      stat: ["shooter", "basic"],
+      options: { },
+    },
+  ],
+}
+
+
+// ENEMIES

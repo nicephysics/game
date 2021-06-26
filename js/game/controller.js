f627cd2647bcae4123087ac7fed364f5d061e8fb
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-06-26 09:49:50 +0800

    Update controller.js

--- js/game/controller.js
+++ js/game/controller.js
@@ -8,6 +8,7 @@ export class Controller {
   
   // fields
   type = "none"
+  thing = null
   
   // constructor
   constructor(thing) {
@@ -41,6 +42,36 @@ export class Controller {
   
   tickPlayer() {
     // todo
+    const t = this.thing,
+          size = t.size
+    Body.setPosition(t.body, math.lerpVector(t.position, t.targetpos, config.smooth.tower.pos))
+    // do control
+    let c = t.control,
+        movedir = Vector.create(0, 0)
+    // move direction
+    if (c.up) movedir.y--
+    if (c.down) movedir.y++
+    if (c.left) movedir.x--
+    if (c.right) movedir.x++
+    // move variables
+    var speed = t.movespeed * (t.effect.speedmult || 0),
+        moveVector = Vector.mult(Vector.normalise(movedir), speed),
+        moveResult = Vector.add(t.targetpos, moveVector)
+    // move limits
+    if (moveResult.x - size < config.movelimits.min.x) moveResult.x = config.movelimits.min.x + size
+    if (moveResult.y - size < config.movelimits.min.y) moveResult.y = config.movelimits.min.y + size
+    if (moveResult.x + size > config.movelimits.max.x) moveResult.x = config.movelimits.max.x - size
+    if (moveResult.y + size > config.movelimits.max.y) moveResult.y = config.movelimits.max.y - size
+    // move!
+    t.targetpos = moveResult
+    // rotate!
+    if (t.effect.canturn) {
+      if (c.autorotate) {
+        t.targetrot += 0.015
+      } else {
+        t.targetrot = Vector.angle(t.position, c.pointer)
+      }
+    }
   }
   
   tickEnemy() {

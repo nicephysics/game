4f7ca798751b23b01b23c01f89b7a5ac7aa4d2e1
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-07-11 12:36:33 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+import { Game } from "./game/game.js"
 import { game_init, game_menu, game_start } from "./game/start.js"
+
+window.addEventListener("load", function() {
+    game_init()
+    game_menu()
+})

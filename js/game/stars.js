5421d754bd69304a990be121e47f1832fda6fa1d
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-07-18 19:58:42 +0800

    Update stars.js

--- js/game/stars.js
+++ js/game/stars.js
@@ -1,1 +1,29 @@
+import { category, config } from "../config/config.js"
 
+import { random } from "../util/random.js"
+import { math } from "../util/math.js"
+
+export var stars = { }
+
+stars.stars = {
+  tutorial: {
+    name: "Proxima Centauri",
+    short: "α Cen C", // actual
+    planets: [
+      { name: "b",
+        full: "α Cen Cb",
+        wave: "tut1",
+        description: "",
+        game_description: "",
+      },
+      { name: "c",
+        full: "α Cen Cc",
+        wave: "tut2",
+        description: "",
+        game_description: "",
+      },
+    ],
+    description: "Proxima Centauri is the nearest star to the Sun, four and a quarter light-years away.",
+    game_description: "",
+  }
+}

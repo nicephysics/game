9684e2ac3c2361aa1bd5a49a545f07aed4439e21
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-07-19 23:12:18 +0800

    Update color.js

--- js/display/color.js
+++ js/display/color.js
@@ -1,1 +1,15 @@
+import { math } from "../util/math.js"
 
+export var color = { }
+export var theme = { }
+
+theme.default = {
+  black: "#000000",
+  white: "#FFFFFF",
+}
+
+theme.set = function(type) {
+  color = theme[type]
+}
+
+theme.set("default")

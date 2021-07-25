609d31ad2a91e293c57908cbdd585cbc31aa2d8d
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-07-25 21:51:00 +0800

    Update color.js

--- js/display/color.js
+++ js/display/color.js
@@ -7,6 +7,7 @@ theme.default = {
   // grayscale colours
   black:         "#000000", // !
   white:         "#ffffff", // !
+  lightgrey:     "#a4a9ba", // planet popup
   grey:          "#6a6e7a", // !
   locked:        "#6a6e7a", // locked
   // red colours
@@ -14,14 +15,16 @@ theme.default = {
   orange:        "#d46a00", // xp bar base color
   // yellow colours
   // green colours
+  darkgreen:     "#114712", // dark forest green, used for text
   // blue colours
   lightblue:     "#add1ff", // blue text probably
-  darkblue:      "#00367d", // blue text probably
+  darkblue:      "#0a3773", // blue text probably
+  deepblue:      "#061a69", // blue text probably
   cyan:          "#13f2d8", // tier up overlay / other overlay
   button:        "#00ffee", // button
   button_hover:  "#00ada2", // button hover
   // purple colours
-  darkpurple:        "#27007a", // icons?
+  darkpurple:    "#27007a", // icons?
   // misc colours
   wow:           "#6EQUJ5", // Wow!
 }

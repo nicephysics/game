b296fbf6812aa955c6bee7dbe46cfe79d4e1aa3f
nicephysics <84841239+nicephysics@users.noreply.github.com>
2021-08-08 23:10:46 +0800

    Update save.js

--- js/game/save.js
+++ js/game/save.js
@@ -3,3 +3,27 @@
 // #define say cout<<
 // using namespace std; // no
 // main() { (((say))) "Hello\xa0World!";;; }
+
+import { Tower } from "./tower.js"
+import { waves } from "./waves.js"
+
+export const save = { }
+
+save.save = {
+  game: { },
+}
+const S = save.save // a constant, for convenience
+
+save.set = function() {
+  localStorage.setItem("game", JSON.stringify(S))
+}
+
+save.get = function() {
+  save.save = JSON.parse(localStorage.getItem("game"))
+}
+
+save.wave = function() {
+  S.game = {
+    player: Tower.saveTower(),
+  }
+}
